171d6edba6d0e38936bb7d8da90276fb1af6351d
DH <DH@DESKTOP-JQU55L7>
2017-08-08 15:44:52 +0900

    Persistence managed, Entity Mapping 정독.

--- JPA_training/src/main/java/com/h066it/dto/Member.java
+++ JPA_training/src/main/java/com/h066it/dto/Member.java
@@ -1,20 +1,43 @@
 package com.h066it.dto;
 
+import java.util.Date;
+
 import javax.persistence.*;
 
 @Entity
-@Table(name = "MEMBER")
+//@SequenceGenerator(
+//		name = "BOARD_SEQ_GENERATOR",
+//		sequenceName = "BOARD_SEQ",
+//		initialValue = 1, allocationSize = 1)
+@Table(name = "MEMBER", uniqueConstraints = {@UniqueConstraint(
+		name = "NAME_AGE_UNIQUE",
+		columnNames = {"NAME", "AGE"}
+		)})
 public class Member {
 
 	@Id
+//	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+//			generator = "BOARD_SEQ_GENERATOR")
 	@Column(name = "ID")
 	private String id;
-	
-	@Column(name = "NAME")
+
+	@Column(name = "NAME", nullable = false, length = 10)
 	private String username;
-	
-	//If mapping name equals null, then field name will set on column name.
+
+	// If mapping name equals null, then field name will set on column name.
 	private int age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	@Lob
+	private String description;
 
 	public String getId() {
 		return id;
@@ -38,5 +61,41 @@ public class Member {
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	public RoleType getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(RoleType roleType) {
+		this.roleType = roleType;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public enum RoleType {
+		ADMIN, UESR
 	}
 }
