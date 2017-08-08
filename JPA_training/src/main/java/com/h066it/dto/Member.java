f2107fa112e54adc858a73fab414de5584a47076
DH <DH@DESKTOP-JQU55L7>
2017-08-08 16:19:39 +0900

    ORM training setting.

--- JPA_training/src/main/java/com/h066it/dto/Member.java
+++ JPA_training/src/main/java/com/h066it/dto/Member.java
@@ -1,101 +1,58 @@
 package com.h066it.dto;
 
-import java.util.Date;
-
 import javax.persistence.*;
 
 @Entity
-//@SequenceGenerator(
-//		name = "BOARD_SEQ_GENERATOR",
-//		sequenceName = "BOARD_SEQ",
-//		initialValue = 1, allocationSize = 1)
-@Table(name = "MEMBER", uniqueConstraints = {@UniqueConstraint(
-		name = "NAME_AGE_UNIQUE",
-		columnNames = {"NAME", "AGE"}
-		)})
 public class Member {
 
 	@Id
-//	@GeneratedValue(strategy = GenerationType.SEQUENCE,
-//			generator = "BOARD_SEQ_GENERATOR")
-	@Column(name = "ID")
-	private String id;
+	@GeneratedValue
+	@Column(name = "MEMBER_ID")
+	private Long id;
 
-	@Column(name = "NAME", nullable = false, length = 10)
-	private String username;
+	private String name;
 
-	// If mapping name equals null, then field name will set on column name.
-	private int age;
+	private String city;
+	private String street;
+	private String zipcode;
 
-	@Enumerated(EnumType.STRING)
-	private RoleType roleType;
-
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date createdDate;
-
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date lastModifiedDate;
-
-	@Lob
-	private String description;
-
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
-	public String getUsername() {
-		return username;
+	public String getName() {
+		return name;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public int getAge() {
-		return age;
+	public String getCity() {
+		return city;
 	}
 
-	public void setAge(int age) {
-		this.age = age;
+	public void setCity(String city) {
+		this.city = city;
 	}
 
-	public RoleType getRoleType() {
-		return roleType;
+	public String getStreet() {
+		return street;
 	}
 
-	public void setRoleType(RoleType roleType) {
-		this.roleType = roleType;
+	public void setStreet(String street) {
+		this.street = street;
 	}
 
-	public Date getCreatedDate() {
-		return createdDate;
+	public String getZipcode() {
+		return zipcode;
 	}
 
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
-
-	public Date getLastModifiedDate() {
-		return lastModifiedDate;
-	}
-
-	public void setLastModifiedDate(Date lastModifiedDate) {
-		this.lastModifiedDate = lastModifiedDate;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public enum RoleType {
-		ADMIN, UESR
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
 	}
 }
--- JPA_training/src/main/java/com/h066it/test/test.java
+++ JPA_training/src/main/java/com/h066it/test/test.java
@@ -17,7 +17,7 @@ public class test {
 		
 		try {
 			tx.begin();
-			logic(em);
+			// empty.
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
@@ -27,32 +27,4 @@ public class test {
 		emf.close();
 	}
 	
-	private static void logic(EntityManager em) {
-		String id = "id1";
-		
-		Member member = new Member();
-		
-		member.setId(id);
-		member.setUsername("ม๖วั");
-		member.setAge(2);
-		
-		//create
-		em.persist(member);
-		
-		//update
-		member.setAge(20);
-		
-		//read
-		Member findMember = em.find(Member.class, id);
-		System.out.println("findMember = " + findMember.getUsername() + 
-			", age = " + findMember.getAge());
-		
-		//read all
-		List<Member> members =
-			em.createQuery("SELECT m FROM Member m", Member.class).getResultList();
-		System.out.println("member.size = " + members.size());
-		
-		//delete
-		em.remove(member);
-	}
 }
